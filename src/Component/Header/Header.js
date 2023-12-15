77dfff5ebf76d2985d57d8aeff2838d04e642ab4
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-15 15:05:03 +0530

    okie

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  {/* <a className="navbar-brand" href="#">Navbar</a> */}
+  <Link className="navbar-brand" to="#">Navbar</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
