cae9b75849bd6278bd8d193eac5dae2de508f111
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-17 15:35:06 +0530

    okie

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -6,9 +6,9 @@ import { Typewriter } from 'react-simple-typewriter'
 const Home = () => {
   return (
     <>
-        <section className="hero" id="home">
+        <section className="home" id="home">
             <div className="container f_flex top">
-                <div className="left-top">
+                <div className="left top">
                     <h1>
                         Hi, I'm <span>Palani Selvam</span>
                     </h1>
@@ -26,27 +26,27 @@ const Home = () => {
                         </span> 
                     </h2>
                     <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
-                    <div className="hero_btn d_flex">
+                    <div className="home_btn d_flex">
                         <div className="col_1">
                             <h4>Find Me</h4>
                             <div className="button">
-                                <div className="btn_shadow">
-                                    <i className="uil uil-instagram-alt"></i>
-                                </div>
-                                <div className="btn_shadow">
+                                <button className="btn_shadow">
                                     <i class="uil uil-github"></i>
-                                </div>
-                                <div className="btn_shadow">
+                                </button>
+                                <button className="btn_shadow">
                                     <i class="uil uil-linkedin"></i>
-                                </div>
+                                </button>
+                                <button className="btn_shadow">
+                                    <i className="uil uil-instagram-alt"></i>
+                                </button>
                             </div>
                         </div>
-                        <div className="right">
-                            <div className="right_img">
-                                <img src={dp} alt="" />
-                            </div>
-                           
-                        </div>
+                        
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="right_img">
+                        <img src={dp} alt="" />
                     </div>
                 </div>
             </div>
