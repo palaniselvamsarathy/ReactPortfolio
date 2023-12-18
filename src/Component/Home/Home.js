2a230384123931efc75a16188d7cb4368d2df671
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-19 00:00:43 +0530

    okie

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -8,54 +8,51 @@ const Home = () => {
   return (
     <>
         
-        <section className="home container-fluid">
-            <div className="row">
-                <div className="col-lg-8 col-md-6 col-sm-12">
-                    <div className="left top">
-                        <h1>
-                            Hi, I'm <span>Palani Selvam</span>
-                        </h1>
-                        <h2>A  
-                            <span> 
-                                <Typewriter
-                                words={[' FrontEnd Developer', ' Fullstack Developer']}
-                                loop
-                                cursor
-                                cursorStyle='|'
-                                typeSpeed={70}
-                                deleteSpeed={50}
-                                delaySpeed={1000}
-                                />
-                            </span> 
-                        </h2>
-                        <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
+        <section className="home">
+            
+                <div className="row">
+                    <div className="col-lg-8 col-md-6 col-sm-12">
+                        <div className="left top">
+                            <h1>
+                                Hi, I'm <span>Palani Selvam</span>
+                            </h1>
+                            <h2>A  
+                                <span> 
+                                    <Typewriter
+                                    words={[' FrontEnd Developer', ' Fullstack Developer']}
+                                    loop
+                                    cursor
+                                    cursorStyle='|'
+                                    typeSpeed={70}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                    />
+                                </span> 
+                            </h2>
+                            <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
+                        </div>
+                        <div className="home_btn d_flex">
+                            <h4>Find Me</h4>
+                            <div className="button">
+                                <button className="btn_shadow">
+                                    <a href="https://github.com/palaniselvamsarathy"><i class="uil uil-github"></i></a>
+                                </button>
+                                <button className="btn_shadow">
+                                    <a href="https://www.linkedin.com/in/palaniselvam-m/"><i class="uil uil-linkedin"></i></a>
+                                </button>
+                                <button className="btn_shadow">
+                                    <i className="uil uil-instagram-alt"></i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="home_btn d_flex">
-                        <h4>Find Me</h4>
-                        <div className="button">
-                            <button className="btn_shadow">
-                                <a href="https://github.com/palaniselvamsarathy"><i class="uil uil-github"></i></a>
-                            </button>
-                            <button className="btn_shadow">
-                                <a href="https://www.linkedin.com/in/palaniselvam-m/"><i class="uil uil-linkedin"></i></a>
-                            </button>
-                            <button className="btn_shadow">
-                                <i className="uil uil-instagram-alt"></i>
-                            </button>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        
+                        <div className="right_img">
+                            <img src={dp} alt="" />
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
-                    <div className="right_img">
-                        <img src={dp} alt="" />
-                    </div>
-                </div>
-            </div> 
-            <div className="row">
-                <div className="col">
-                
-                </div>
-            </div>
             
         </section>
     </>
