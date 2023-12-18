61ffe163ca8efb22457120e31d5379163ce11bf8
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-18 20:06:36 +0530

    okie

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,56 +1,13 @@
 import React from 'react'
 import "./Home.css"
 import dp from "../../assets/DP2.png"
+
 import { Typewriter } from 'react-simple-typewriter'
 
 const Home = () => {
   return (
     <>
-        {/* <section className="home" id="home">
-            <div className="container f_flex top">
-                <div className="left top">
-                    <h1>
-                        Hi, I'm <span>Palani Selvam</span>
-                    </h1>
-                    <h2>A  
-                        <span> 
-                            <Typewriter
-                            words={[' FrontEnd Developer', ' Fullstack Developer']}
-                            loop
-                            cursor
-                            cursorStyle='|'
-                            typeSpeed={70}
-                            deleteSpeed={50}
-                            delaySpeed={1000}
-                            />
-                        </span> 
-                    </h2>
-                    <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
-                    <div className="home_btn d_flex">
-                        <div className="col_1">
-                            <h4>Find Me</h4>
-                            <div className="button">
-                                <button className="btn_shadow">
-                                    <i class="uil uil-github"></i>
-                                </button>
-                                <button className="btn_shadow">
-                                    <i class="uil uil-linkedin"></i>
-                                </button>
-                                <button className="btn_shadow">
-                                    <i className="uil uil-instagram-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className="right">
-                    <div className="right_img">
-                        <img src={dp} alt="" />
-                    </div>
-                </div>
-            </div>
-        </section> */}
+        
         <section className="home container-fluid">
             <div className="row">
                 <div className="col-lg-8 col-md-6 col-sm-12">
@@ -71,15 +28,16 @@ const Home = () => {
                                 />
                             </span> 
                         </h2>
+                        <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
                     </div>
                     <div className="home_btn d_flex">
                         <h4>Find Me</h4>
                         <div className="button">
                             <button className="btn_shadow">
-                                <i class="uil uil-github"></i>
+                                <a href="https://github.com/palaniselvamsarathy"><i class="uil uil-github"></i></a>
                             </button>
                             <button className="btn_shadow">
-                                <i class="uil uil-linkedin"></i>
+                                <a href="https://www.linkedin.com/in/palaniselvam-m/"><i class="uil uil-linkedin"></i></a>
                             </button>
                             <button className="btn_shadow">
                                 <i className="uil uil-instagram-alt"></i>
@@ -91,14 +49,14 @@ const Home = () => {
                     <div className="right_img">
                         <img src={dp} alt="" />
                     </div>
-                    {/* <img src={dp} alt="" /> */}
+                </div>
+            </div> 
+            <div className="row">
+                <div className="col">
+                
                 </div>
             </div>
-            {/* <div className="row">
-                <div className="col">
-                <p> I am the person who loves turning ideas into cool websites. I specialize in making things look great on the front end and work seamlessly behind the scenes with smart, clean code. Let's bring your ideas to life! </p>
-                </div>
-            </div> */}
+            
         </section>
     </>
   )
