6ed43b9f68a80eb42e3038d942fe189b47823e82
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-29 14:14:39 +0530

    okie

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Switch} from 'react-router-dom'
-import Header from './Component/Header/Header'
-import Home from './Component/Home/Home'
-import About from './Component/About/About'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Navbar from './Component/Navbar/Navbar'
+// import Header from './Component/Header/Header'
+// import Home from './Component/Home/Home'
+// import About from './Component/About/About'
 
 const App = () => {
   return (
     <div>
       <Router>
-      <Header/>
-      <Home/>
-      <About/>
+      {/* <Header/> */}
+      {/* <Home/>
+      <About/> */}
+      <Navbar/>
       </Router>
       
     </div>
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import "./Header.css"
-import Darkmode from '../Dark/Darkmode'
-const Header = () => {
+// import "./Header.css"
+import "./Navbar.css"
+// import Darkmode from '../Dark/Darkmode'
+const Navbar= () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link to="/" className='navbar-brand ml-5'><span className='logo'>P</span>alani <span className='logo'>S</span>elvam</Link>
-        <Darkmode/>
+        {/* <Darkmode/> */}
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,19 +16,19 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <Link to="/" className='nav-link'>Home <span className="sr-only">(current)</span></Link>
+              <Link to="/" className='nav-link link'>Home</Link>
             </li>
             <li className="nav-item active">
-              <Link to="/" className='nav-link'>About <span className="sr-only">(current)</span></Link>
+              <Link to="/" className='nav-link link'>About</Link>
             </li>
             <li className="nav-item active">
-              <Link to="/" className='nav-link'>Skills <span className="sr-only">(current)</span></Link>
+              <Link to="/" className='nav-link link'>Skills</Link>
             </li>
             <li className="nav-item active">
-              <Link to="/" className='nav-link'>Projects <span className="sr-only">(current)</span></Link>
+              <Link to="/" className='nav-link link'>Projects</Link>
             </li>
             <li className="nav-item active">
-              <Link to="/" className='nav-link'>Contact <span className="sr-only">(current)</span></Link>
+              <Link to="/" className='nav-link link'>Contact</Link>
             </li>
           </ul>
         </div>
@@ -36,4 +37,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Navbar
