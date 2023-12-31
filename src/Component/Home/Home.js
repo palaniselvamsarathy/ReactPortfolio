b0f4d8926915fa12315993ac10e6dbdf1d017d16
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-31 20:50:56 +0530

    okie

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -54,8 +54,43 @@ const Home = () => {
         </section> */}
         <section className="home">
             <div className="row">
-                <div className="col"></div>
-                <div className="col"></div>
+                <div className="col-lg-8 col-md-6 col-sm-12">
+                    <div className="left top">
+                        <h1>
+                            Hi, I'm <span>Palani Selvam</span>
+                        </h1>
+                        <h2> A  
+                            <span> 
+                                <Typewriter
+                                words={[' FrontEnd Developer', ' Fullstack Developer']}
+                                loop
+                                cursor
+                                cursorStyle='|'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                                />
+                            </span> 
+                        </h2>
+                        <h4>Find Me</h4>
+                        <div className="button">
+                            <button className="btn_shadow">
+                                <a href="https://github.com/palaniselvamsarathy"><i class="uil uil-github"></i></a>
+                            </button>
+                            <button className="btn_shadow">
+                                <a href="https://www.linkedin.com/in/palaniselvam-m/"><i class="uil uil-linkedin"></i></a>
+                            </button>
+                            <button className="btn_shadow">
+                                <i className="uil uil-instagram-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12">
+                    <div className="right_img">
+                        <img src={dp} alt="" />
+                    </div>
+                </div>
             </div>
         </section>
     </>
