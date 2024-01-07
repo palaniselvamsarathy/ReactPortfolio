c7bc9c71983eea46022c18104c57d55a9246e380
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-01-07 15:01:25 +0530

    okie

---
+++ backup/Component copy/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar Component</h1>
+      <nav>
+        <h1>Logo</h1>
+        <div>
+          <ul>
+            <Link to=""><li>Home</li></Link>
+            <Link to=""><li>Home</li></Link>
+            <Link to=""><li>Home</li></Link>
+            <Link to=""><li>Home</li></Link>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
