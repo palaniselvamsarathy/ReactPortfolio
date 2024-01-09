d09444467633a910a537c9c23f86f476f7b69bc8
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-01-09 11:43:23 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./App.css"
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Navbar from './Pages/Home/Navbar'
 import Home from './Pages/Home/HomeScreen'
+
 const App = () => {
   return (
     <div className='App'>
+      <Router>
+        <div>
+          <Navbar/>
+        </div>
+      </Router>
       <Home/>
     </div>
   )
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
+  const [navActice,setNavActive] = useState(false);
+  const toggleNav = ()=>{
+    setNavActive(!navActice)
+  }
+  const closeMenu = ()=>{
+    setNavActive(false)
+  }
+  useEffect(()=>{
+    const handleResize =()=>{
+      if (window.innerWidth <= 500){
+        closeMenu()
+      }
+    }
+
+    window.addEventListener("resize",handleResize);
+
+    return ()=>{
+      window.removeEventListener("resize",handleResize);
+    }
+  },[]);
+
+  useEffect(()=>{
+    if(window.innerWidth<=1200){
+      closeMenu();
+    }
+  },[]);
+
   return (
     <div>
-      
+      <nav className={`navbar ${navActice ? "active":""}`}>
+        <div>
+          <img src="./img/main.png" alt="My Logo" />
+        </div>
+      </nav>
     </div>
   )
 }
