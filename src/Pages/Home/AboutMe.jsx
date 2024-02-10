ca9c8fefb24d512735f99beff866948eddc1f7e7
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-10 20:05:40 +0530

    okie

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -15,7 +15,8 @@ function AboutMe() {
             About Me
           </h1>
           <p className='hero-section-description'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error possimus saepe porro eum minus iusto nulla atque culpa, assumenda dignissimos aperiam eius, inventore ducimus expedita libero veniam ad id vel sit voluptates! Dicta quisquam nisi doloremque aperiam tenetur vero eum? Provident hic voluptatibus sed veniam iure deserunt quos soluta.
+            I am a self-taught junior web developer who dedicated three months to mastering web technologies. During this time, I successfully completed projects that include a personalized portfolio highlighting my skills and a fully functional e-commerce website. 
+             Additionally, I expanded my skill set by delving into Python and SQL through comprehensive online learning resources. I am passionate about leveraging technology to create innovative and seamless digital experiences.
           </p>
           {/* <p className='hero-section-description'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error possimus saepe porro eum minus iusto nulla atque culpa, assumenda dignissimos aperiam eius, inventore ducimus expedita libero veniam ad id vel sit voluptates! Dicta quisquam nisi doloremque aperiam tenetur vero eum? Provident hic voluptatibus sed veniam iure deserunt quos soluta.
---
+++ src/Pages/Home/MyPortfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyPortfolio() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyPortfolio
