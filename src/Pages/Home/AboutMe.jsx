b6c0c62c80e3fd400ffe2885135680ec7be14b55
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-10 19:31:07 +0530

    okie

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 function AboutMe() {
   return (
-    <div>
+    <section id='AboutMe' className='about-section'>
+      <div className="about-section-img">
+        <img src="./img/DP1.jpeg" alt="About Me" />
+      </div>
+      <div className="hero-section-content-box about-section-box">
+        <div className="hero-section-content">
+          <p className="section-title">
+            About
+          </p>
+          <h1 className="skills-section-heading">
+            About Me
+          </h1>
+          <p className='hero-section-description'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error possimus saepe porro eum minus iusto nulla atque culpa, assumenda dignissimos aperiam eius, inventore ducimus expedita libero veniam ad id vel sit voluptates! Dicta quisquam nisi doloremque aperiam tenetur vero eum? Provident hic voluptatibus sed veniam iure deserunt quos soluta.
+          </p>
+          {/* <p className='hero-section-description'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi error possimus saepe porro eum minus iusto nulla atque culpa, assumenda dignissimos aperiam eius, inventore ducimus expedita libero veniam ad id vel sit voluptates! Dicta quisquam nisi doloremque aperiam tenetur vero eum? Provident hic voluptatibus sed veniam iure deserunt quos soluta.
+          </p> */}
+        </div>
+      </div>
       
-    </div>
+    </section>
   )
 }
 
