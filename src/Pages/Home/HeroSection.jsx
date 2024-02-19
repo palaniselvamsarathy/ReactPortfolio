b804c5a2f91047363c2174779d2174c44d45053c
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-19 20:00:12 +0530

    changes

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -16,9 +16,15 @@ function HeroSection() {
                 </h1>
                 <p className='hero-section-description'>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ab?
-                    <br />
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, error.
                 </p>
+                <div id="socials-container">
+                  <a href="https://www.linkedin.com/in/palaniselvam-m/">
+                    <img src="./img/linkedin.png" alt="My LinkedIn profile" class="icon" />
+                  </a>
+                  <a href="https://github.com/palaniselvamsarathy" >
+                    <img src="./img/github.png" alt="My Github profile" class="icon" />
+                  </a>
+                </div>
             </div>
             <button className='btn btn-primary'>Get In Touch</button>
         </div>
